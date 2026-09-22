0d12020479f2610a9ff979c51414e566dbcd786a
jorabase <fahim1mia1@gmail.com>
2026-09-22 20:13:51 +0600

    build: downgrade kotlin and ksp versions
    Lower Kotlin to 2.1.10 and corresponding KSP version to ensure compatibility, while lowering minSdk to 23 and updating build properties.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
   defaultConfig {
     applicationId = "com.aistudio.maxbird.bxklpq"
-    minSdk = 24
+    minSdk = 23
     targetSdk = 36
     versionCode = 1
     versionName = "1.0"
